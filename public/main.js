41fff28aebff8f8be0dc17154ae42841d516cc14
AniketBhute <aniket.bhute96@gmail.com>
2020-05-30 22:33:47 +0530

    Add files via upload
    First Commit

---
+++ public/main.js
@@ -0,0 +1,86 @@
+let peer = require('simple-peer');
+let socket = io()
+
+const video = document.querySelector('video')
+
+let client = {}
+
+// Get the video stream 
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(stream => {
+
+        socket.emit('NewClient');
+        video.srcObject = stream;
+        video.play();
+
+        //used to initialize a peer
+        function InitPeer(type) {
+            let peer = new Peer({ initiator: (type == 'init') ? true : false, stream: stream, trickle: false })
+            peer.on('stream', function (stream) {
+                CreateVideo(stream)
+            })
+            //This isn't working in chrome; works perfectly in firefox.
+            // peer.on('close', function () {
+            //     document.getElementById("peerVideo").remove();
+            //     peer.destroy()
+            // })
+
+            peer.on('data', function (data) {
+                let decodedData = new TextDecoder('utf-8').decode(data)
+                let peervideo = document.querySelector('#peerVideo')
+                peervideo.style.filter = decodedData
+            })
+            return peer
+        }
+
+        //for peer of type init
+        function MakePeer() {
+            client.gotAnswer = false
+            let peer = InitPeer('init')
+            peer.on('signal', function (data) {
+                if (!client.gotAnswer) {
+                    socket.emit('Offer', data)
+                }
+            })
+            client.peer = peer
+        }
+
+        //for peer of type not init
+        function FrontAnswer() {
+
+            let peer = InitPeer('notInit')
+            peer.on('signal', (data) => {
+                socket.emit('Answer', data)
+            })
+            peer.signal(offer)
+            client.peer = peer
+
+        }
+
+        function SignalAnswer(answer) {
+            client.gotAnswer = true
+            let peer = client.peer
+            peer.signal(answer)
+        }
+
+        function CreateVideo(stream){
+            let video = document.createElement('video');
+            video.id = 'peerVideo';
+            video.srcObject = stream;
+            video.class = 'embed-responsive-item';
+            document.querySelector('#peerDiv').appendChild(video);
+            video.play()
+        }
+
+        function SessionActive(){
+            document.write('Session Active. Please come back later')
+        }
+
+        socket.on('BackOffer', FrontAnswer)
+        socket.on('BackAnswer', SignalAnswer)
+        socket.on('SessionActive', SessionActive)
+        socket.on('CreatePeer', MakePeer)
+        // socket.on('Disconnect', RemovePeer)
+
+    })
+    .catch(err => document.write(err))
