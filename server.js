0e45772c474e32e42551e5167d8f3a7474b5f14c
aniketpdgc <aniket.bhute@pdgc.com>
2020-05-30 23:20:46 +0530

    Changed PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const port = process.env.port || 4000
+const port = process.env.PORT || 4000
 
 app.use(express.static(__dirname + "/public"))
 let clients = 0;
